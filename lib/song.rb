f91e49a6802c744f6c2b5903ef8cc980b2610bee
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-11 18:05:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,9 @@
 class Song 
-  
-  @@songcount = 0
-  
+
   def new(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
-  
-  def count
-    @@songcount
-  end
-  
+
 end
